29d290d511252320883b72beba8d31a675e8a521
korosu <dmshijie@gmail.com>
2023-12-13 17:01:58 +0100

    different path

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 import { Suspense } from "react";
 import RandomName from "./randomName";
-import RevalidateButton from "./revalidateButton";
 export default function Home() {
   return (
     <div className="max-w-md">
       <Suspense fallback={<p>Laoding...</p>}>
         <RandomName></RandomName>
       </Suspense>
-      <RevalidateButton></RevalidateButton>
     </div>
   );
 }
